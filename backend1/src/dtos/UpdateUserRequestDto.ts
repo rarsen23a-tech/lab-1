4a04515ddf365ad000c2a14764ab5a3f24d58937
Arsen <r.arsen23.a@gmail.com>
2026-05-26 21:36:42 +0300

    Add email field to UpdateUserRequestDto

--- backend1/src/dtos/UpdateUserRequestDto.ts
+++ backend1/src/dtos/UpdateUserRequestDto.ts
@@ -1,15 +1,15 @@
 type UpdateUserRequest = {
     name?: unknown;
+    email?: unknown;
 };
 
 class UpdateUserRequestDto {
     public name?: string;
+    public email?: string;
 
     constructor(data: UpdateUserRequest = {}) {
-        this.name =
-            typeof data.name === 'string'
-                ? data.name.trim()
-                : undefined;
+        this.name = typeof data.name === 'string' ? data.name.trim() : undefined;
+        this.email = typeof data.email === 'string' ? data.email.trim() : undefined;
     }
 }
 
