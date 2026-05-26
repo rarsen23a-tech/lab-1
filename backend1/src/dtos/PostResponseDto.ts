707235d6c0c8ddb821b51e947cc264ba6f1c3867
Arsen <r.arsen23.a@gmail.com>
2026-05-26 21:35:56 +0300

    Add userId and createdAt to PostResponseDto

--- backend1/src/dtos/PostResponseDto.ts
+++ backend1/src/dtos/PostResponseDto.ts
@@ -2,28 +2,23 @@ type Post = {
     id?: unknown;
     title?: unknown;
     content?: unknown;
+    userId?: unknown;
+    createdAt?: unknown;
 };
 
 class PostResponseDto {
     public id: number;
     public title: string;
     public content: string;
+    public userId: number;
+    public createdAt: string;
 
     constructor(post: Post = {}) {
-        this.id =
-            typeof post.id === 'number'
-                ? post.id
-                : 0;
-
-        this.title =
-            typeof post.title === 'string'
-                ? post.title
-                : '';
-
-        this.content =
-            typeof post.content === 'string'
-                ? post.content
-                : '';
+        this.id = typeof post.id === 'number' ? post.id : 0;
+        this.title = typeof post.title === 'string' ? post.title : '';
+        this.content = typeof post.content === 'string' ? post.content : '';
+        this.userId = typeof post.userId === 'number' ? post.userId : 0;
+        this.createdAt = typeof post.createdAt === 'string' ? post.createdAt : '';
     }
 }
 
