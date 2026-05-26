118f94f2cb544c0ef1c37034bb9e2f4f36e100e9
Arsen <r.arsen23.a@gmail.com>
2026-05-26 21:37:07 +0300

    Add email and createdAt fields to UserResponseDto

--- backend1/src/dtos/UserResponseDto.ts
+++ backend1/src/dtos/UserResponseDto.ts
@@ -1,22 +1,21 @@
 type User = {
     id?: unknown;
     name?: unknown;
+    email?: unknown;
+    createdAt?: unknown;
 };
 
 class UserResponseDto {
     public id: number;
     public name: string;
+    public email: string | null;
+    public createdAt: string;
 
     constructor(user: User = {}) {
-        this.id =
-            typeof user.id === 'number'
-                ? user.id
-                : 0;
-
-        this.name =
-            typeof user.name === 'string'
-                ? user.name
-                : '';
+        this.id = typeof user.id === 'number' ? user.id : 0;
+        this.name = typeof user.name === 'string' ? user.name : '';
+        this.email = typeof user.email === 'string' ? user.email : null;
+        this.createdAt = typeof user.createdAt === 'string' ? user.createdAt : '';
     }
 }
 
