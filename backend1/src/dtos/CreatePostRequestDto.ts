a606d722f22e38e390346f579b3cdcfda383b89b
Arsen <r.arsen23.a@gmail.com>
2026-05-28 01:49:52 +0300

    Update CreatePostRequestDto.ts

--- backend1/src/dtos/CreatePostRequestDto.ts
+++ backend1/src/dtos/CreatePostRequestDto.ts
@@ -1,29 +1,21 @@
 type CreatePostRequest = {
     title?: unknown;
     content?: unknown;
+    category?: unknown;
     userId?: unknown;
 };
 
 class CreatePostRequestDto {
     public title: string;
     public content: string;
+    public category: string | undefined;
     public userId: number;
 
     constructor(data: CreatePostRequest) {
-        this.title =
-            typeof data.title === 'string'
-                ? data.title.trim()
-                : '';
-
-        this.content =
-            typeof data.content === 'string'
-                ? data.content.trim()
-                : '';
-
-        this.userId =
-            typeof data.userId === 'number'
-                ? data.userId
-                : Number(data.userId);
+        this.title = typeof data.title === 'string' ? data.title.trim() : '';
+        this.content = typeof data.content === 'string' ? data.content.trim() : '';
+        this.category = typeof data.category === 'string' ? data.category.trim() : undefined;
+        this.userId = typeof data.userId === 'number' ? data.userId : Number(data.userId);
     }
 }
 
