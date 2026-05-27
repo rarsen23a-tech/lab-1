ecf373b03a05b619c5645e7d02a8e982ad23f2ae
Arsen <r.arsen23.a@gmail.com>
2026-05-28 01:51:57 +0300

    Update UpdatePostRequestDto.ts

--- backend1/src/dtos/UpdatePostRequestDto.ts
+++ backend1/src/dtos/UpdatePostRequestDto.ts
@@ -1,22 +1,18 @@
 type UpdatePostRequest = {
     title?: unknown;
     content?: unknown;
+    category?: unknown;
 };
 
 class UpdatePostRequestDto {
     public title?: string;
     public content?: string;
+    public category?: string;
 
     constructor(data: UpdatePostRequest = {}) {
-        this.title =
-            typeof data.title === 'string'
-                ? data.title.trim()
-                : undefined;
-
-        this.content =
-            typeof data.content === 'string'
-                ? data.content.trim()
-                : undefined;
+        this.title = typeof data.title === 'string' ? data.title.trim() : undefined;
+        this.content = typeof data.content === 'string' ? data.content.trim() : undefined;
+        this.category = typeof data.category === 'string' ? data.category.trim() : undefined;
     }
 }
 
